5e1b208a555b6140196e04ca41739e4523d7d068
JasonPinet <jason.hu@pinet.co>
2015-07-20 14:14:49 +0800

    wechat share address

--- wxapi/wxconfig.php
+++ wxapi/wxconfig.php
@@ -7,17 +7,17 @@ class Wxconfig
 {
 	//=======【基本信息设置】=====================================
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-	const APPID = 'wxf0c1d6198e106e87';
+	const APPID = '';
 	//受理商ID，身份标识
-	const MCHID = '10036873';
+	const MCHID = '';
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	const KEY = 'CmPpSO7c3yeeaXag6A1iNjpzS1mRgMTf';
+	const KEY = '';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-	const APPSECRET = '6f963a608d889f18b90c20cd62d82e85';
+	const APPSECRET = '';
 	
 	//=======【JSAPI路径设置】===================================
 	//获取access_token过程中的跳转uri，通过跳转将code传入jsapi支付页面
-	const JS_API_CALL_URL = 'http://wxfx.pinet.cc/shareaddress';
+	const JS_API_CALL_URL = 'http://www.xxx.com/shareaddress';
 	
 	//=======【证书路径设置】=====================================
 	//证书路径,注意应该填写绝对路径
